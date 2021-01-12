5e71a0e6e1d99b244ac0e5e2097673bbdbbe2b3a
向 <920526557@qq.com>
2021-01-12 09:17:52 +0800

    vue-cli

---
+++ src/components/login.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="login_container">
+        <div class="login_box">
+            <!-- 头像区域 -->
+            <div class="avatar_box">
+                <img src="../assets/logo.png" />
+            </div>
+            <!-- 表单区域 -->
+            <el-form label-width="0" :model="loginFrom" class="login_from">
+                <!-- 用户名 -->
+                <el-form-item>
+                    <el-input v-model="loginFrom.username" prefix-icon="iconfont icon-user"></el-input>
+                </el-form-item>
+                <!-- 密码 -->
+                <el-form-item>
+                    <el-input v-model="loginFrom.password" type="password" prefix-icon="iconfont icon-3702mima"></el-input>
+                </el-form-item>
+                <!-- 按钮区域 -->
+                <el-form-item class="btns">
+                    <el-button type="primary">登陆</el-button>
+                    <el-button type="info">重置</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // 这是登陆表单的数据绑定对象
+      loginFrom: {
+        username: '111',
+        password: '111'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+    background-color: #2b4b6b;
+}
+.login_box {
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    margin-left: -250px;
+    margin-top: -150px;
+    .avatar_box {
+        width: 100px;
+        height: 100px;
+        border: 1px solid #eee;
+        border-radius: 50%;
+        padding: 10px;
+        box-shadow: 0 0 10px #ddd;
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background-color: white;
+        img {
+            width: 100%;
+            height: 100px;
+            border-radius: 50%;
+            background-color: #eee;
+        }
+    }
+}
+.btns {
+    display: flex;
+    justify-content: flex-end;
+}
+.login_from {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import login from '../components/login.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-
-]
-
 const router = new VueRouter({
-  routes
+  routes: [
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: login }
+  ]
 })
 
 export default router
