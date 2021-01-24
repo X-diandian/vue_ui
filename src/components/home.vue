d3b646c7a65606bd6bcb8392d3c6d53a4b1b55a9
向 <920526557@qq.com>
2021-01-24 18:52:26 +0800

    this. is not a function报错

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,13 +1,19 @@
 <template>
-    <div>
-        <el-button type="info" @click="logout()">退出</el-button>
-    </div>
+
+    <el-container>
+      <el-header>Header<el-button type="info" @click="logout()">退出</el-button></el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+
 </template>
 
 <script>
 export default {
   methods: {
-    logout: function() {
+    logout: function () {
       window.sessionStorage.clear()
       this.$router.push('/login')
     }
@@ -16,5 +22,32 @@ export default {
 </script>
 
 <style lang='less' scoped>
-
+.el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
 </style>
